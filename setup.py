1551caa68909cc7c0f77818d1c4a39f476c89536
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-10-30 14:50:15 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'pyramid==1.5.7',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy==1.0.8',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'wtforms==2.0.2',
+    'webhelpers2==2.0',
+    'paginate==0.5',
+    'paginate_sqlalchemy==0.2.0'
+]
+dependency_links = [
+]
+
+if __name__ == '__main__':
+    setup(
+        name='WeWallet',
+        version='0.1',
+        packages=find_packages('.'),
+        install_requires=install_requires,
+        dependency_links=dependency_links,
+        include_package_data=True,
+        entry_points="""\
+        """,
+    )
